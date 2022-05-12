fa284c9dca7c86eafc0fc95e2b386d9adb497628
DavidTu <a3102a123@gmail.com>
2022-05-12 22:18:18 +0800

    Complete with comment

--- HW5.py
+++ HW5.py
@@ -12,10 +12,11 @@ import time
 
 # global variable
 beta = 5
-kernel_folder = "K_add"
-new_kernel_folder = "K_add"
+kernel_folder = "K_mul"
+new_kernel_folder = "K_mul"
 img_dir = "image"
 is_new_file = True
+is_SVM = True
 
 is_test = False
 test_num = 50
@@ -35,6 +36,7 @@ def read_point():
     X = np.array(X)
     Y = np.array(Y)
     return X,Y
+
 ### optimization
 def optimization(X,Y):
     theta = np.array([1,1])
@@ -43,11 +45,11 @@ def optimization(X,Y):
     print(res.success)
     print(res.x)
     return res.x
+
 ### kernel function
 def kernel(a,b,theta):
+    # RBF kernel(Radial basis kernel)
     k = theta[0] * np.exp(-theta[1] * ((a-b) ** 2) / 2)
-    # k = npd.euclidean(a,b)
-    # k = np.sqrt((a-b)**2)
     return k
 
 def obj_fun(a,b):
@@ -62,35 +64,39 @@ def log_li(X,Y):
 
 def log_likelihood(X,Y):
     noise = np.diag(np.ones(len(X))) * (1 / beta)
-    f = lambda x: -( -np.log(np.linalg.det(covar(X,X,[x[0],x[1]]) + noise)) / 2 - (Y.T @ np.linalg.inv(covar(X,X,[x[0],x[1]]) + noise) @ Y) / 2 - (len(Y)/2*np.log(np.pi * 2)))
+    f = lambda x: -( -np.log(np.linalg.det(covar(X,X,[x[0],x[1]]) + noise)) / 2 \
+    - (Y.T @ np.linalg.inv(covar(X,X,[x[0],x[1]]) + noise) @ Y) / 2 - (len(Y)/2*np.log(np.pi * 2)))
     return f
 
+# Calculating the covariance matrix
 def covar(X1,X2,theta):
-    global beta
     cov = np.zeros((len(X1),len(X2)))
     for i,x1 in enumerate(X1):
         for j,x2 in enumerate(X2):
             cov[i,j] = kernel(x1,x2,theta)
     return cov
 
-def GP(X,Y,theta):
+def GP(X,Y,theta,title):
+    # prepare model parameter
     noise = np.diag(np.ones(len(X))) * (1 / beta)
-    # theta = [1,1]
     C = covar(X,X,theta) + noise
     C_inv = np.linalg.inv(C)
+
+    # draw model prediction
     line_X = np.linspace(-60,60,num=1000)
     line_Y = np.zeros(len(line_X))
     line_var = np.zeros(len(line_X))
     for i,x in enumerate(line_X):
         line_Y[i],line_var[i] = GP_predict(X,Y,C_inv,x,theta)
-    line_var = 2*abs(line_var)
+    line_var = 2*np.sqrt(abs(line_var))
     plt.figure(figsize=(8, 6), dpi=120)
-    plt.title("Gaussian Process")
+    plt.title(title)
     plt.ylim(-5,5)
     plt.fill_between(line_X,line_Y + line_var,line_Y - line_var,color="yellow")
     plt.plot(line_X,line_Y)
     plt.scatter(X,Y)
 
+# prediction
 def GP_predict(X,Y,C_inv,x_new,theta):
     noise = 1 / beta 
     K = covar(X,[x_new],theta)
@@ -115,6 +121,7 @@ def change_label(Y,num):
     new_Y[Y != num] = -1
     return new_Y
 
+# user defined kernel function , multiply the kernel of RBF & linear
 def SVM_kernel(a,b):
     # according to the origin setting of libsvm
     global gamma
@@ -122,11 +129,12 @@ def SVM_kernel(a,b):
     a = a / np.linalg.norm(a)
     b = b / np.linalg.norm(b)
     linear = np.dot(a,b)
-    # return rbf*linear
+    return rbf*linear
     return (rbf + linear) / 2
 
 pre_name = 0
 name = "_mul"
+# pre-computed kernel
 def X_to_kernel(X1,X2):
     global pre_name
     K = np.zeros((len(X1),len(X2) + 1))
@@ -177,6 +185,8 @@ def SVM(X_train,Y_train,X_test,Y_test):
         KX_train,Y_train = load_SVM_kernel(os.path.join(kernel_folder,"train"))
         KX_test,Y_test = load_SVM_kernel(os.path.join(kernel_folder,"test"))
     print("Kernel data prepared done")
+    kernel_name = ["linear" , "polynomial" , "RBF"]
+    color = ["red","blue","yellow"]
     # s : the type of SVM , 0 : C-SVC , 1 : nu-SVC , 2 : one-class SVM
     # t : the type of kernel , 0 : linear , 1 : polynomial , 2 : RBF(radial basis function)
     # --- 4 : precomputed kernel
@@ -185,26 +195,97 @@ def SVM(X_train,Y_train,X_test,Y_test):
     # user defined kernel multi-class SVM
     prob  = svm_problem(Y_train, KX_train, isKernel=True)
     param = svm_parameter("-s 0 -t 4 -q")
-    # model_user = svm_train(prob,param)
-    # r_label , r_acc, r_val = svm_predict(Y_test,KX_test,model_user)
-    # print(r_acc , r_label)
+    model_user = svm_train(prob,param)
+    r_label , r_acc, r_val = svm_predict(Y_test,KX_test,model_user)
+    print(r_acc)
     # return
 
 
     # multi-class SVM
-    for m in range(1):
-        for c in range(1,5):
-            model_mul = svm_train(Y_train,X_train,"-s 0 -t {} -c {} -v 2 -q".format(m,c))
+    # grid search
+    best_m = 0 ; best_c = 0 ; best_acc = 0
+    acc_arr = np.zeros((3,10))
+    for m in range(3):
+        for c in range(1,10):
+            acc = svm_train(Y_train,X_train,"-s 0 -t {} -c {} -v 2 -q".format(m,c))
+            acc_arr[m,c] = acc
+            if best_acc < acc:
+                best_m = m
+                best_c = c
 
-    return
+    print("best kernel : {} , C = {}".format(kernel_name[best_m],best_c))
+    model_mul = svm_train(Y_train,X_train,"-s 0 -t {} -c {} -q".format(best_m,best_c))
     r_label , r_acc, r_val = svm_predict(Y_test,X_test,model_mul)
-    r_label = np.array(r_label)
-    true_table = (r_label == Y_test)
     print(r_acc)
-    print("{} / {}".format(true_table.sum(),len(Y_test)))
-    # the label of this MNIST begins from 1 to 5
-    for i in range(1,6):
-        print_SVM_result(r_label,Y_test,i,i-1)
+
+    plt.figure()
+    plt.xlabel("C number")
+    plt.ylabel("Accuracy")
+    plt.xticks(range(0,10,1))
+    for m in range(3):
+        y = acc_arr[m,:]
+        plt.plot(range(1,10),y,'s-',color = color[m], label=kernel_name[m])
+    plt.legend(loc = "best")
+
+    # return
+
+    acc_arr = np.zeros(3)
+    acc_n_arr = np.zeros((3,5))
+    sens_n_arr = np.zeros((3,5))
+    spec_n_arr = np.zeros((3,5))
+    # using different kernel for multiclass SVM
+    for m in range(3):
+        print("Using kernel : {}".format(kernel_name[m]))
+        # training multi class SVM
+        model_mul = svm_train(Y_train,X_train,"-s 0 -t {} -q".format(m))
+        r_label , r_acc, r_val = svm_predict(Y_test,X_test,model_mul)
+        r_label = np.array(r_label)
+        true_table = (r_label == Y_test)
+        acc_arr[m] = r_acc[0]
+        print(r_acc)
+        print("{} / {}".format(true_table.sum(),len(Y_test)))
+        # the label of this MNIST begins from 1 to 5
+        for i in range(1,6):
+            acc,sens,spec = print_SVM_result(r_label,Y_test,i,i-1)
+            acc_n_arr[m,i-1] = acc
+            sens_n_arr[m,i-1] = sens
+            spec_n_arr[m,i-1] = spec
+    # draw total accuracy
+    plt.figure()
+    plt.xlabel("Kernel")
+    plt.ylabel("Accuracy")
+    for m in range(3):
+        y = acc_arr[m]
+        plt.bar(kernel_name[m],y,color=color[m])
+        plt.text(m,y,'%s'%y,ha='center')
+    plt.legend(loc = "best")
+
+    plt.figure()
+    plt.xlabel("The label of image")
+    plt.ylabel("Accuracy")
+    plt.xticks(range(0,5,1))
+    for m in range(3):
+        y = acc_n_arr[m,:]
+        plt.plot(range(5),y,'s-',color = color[m], label=kernel_name[m])
+    plt.legend(loc = "best")
+
+    plt.figure()
+    plt.xlabel("The label of image")
+    plt.ylabel("Sensitivity")
+    plt.xticks(range(0,5,1))
+    for m in range(3):
+        y = sens_n_arr[m,:]
+        plt.plot(range(5),y,'s-',color = color[m], label=kernel_name[m])
+    plt.legend(loc = "best")
+
+    plt.figure()
+    plt.xlabel("The label of image")
+    plt.ylabel("Specificity")
+    plt.xticks(range(0,5,1))
+    for m in range(3):
+        y = spec_n_arr[m,:]
+        plt.plot(range(5),y,'s-',color = color[m], label=kernel_name[m])
+    plt.legend(loc = "best")
     return
 
     # one-class SVM 
@@ -237,10 +318,15 @@ def print_SVM_result(D1,D2,label,num_str):
     print("{:15}{:^20}{:^20}".format("Is number {}".format(label),TP,FN))
     print("{:15}{:^20}{:^20}".format("Is not number {}".format(label),FP,TN))
     print("")
-    print("Sensitivity (Successfully predict number {}): {:.5}".format(label,TP / (TP + FN)))
-    print("Specificity (Successfully predict not number {}): {:.5}".format(label,TN / (FP + TN)))
+    acc = (TP+TN) / len(D1)
+    sens = TP / (TP + FN)
+    spec = TN / (FP + TN)
+    print("Accuracy : {:.5}".format(acc))
+    print("Sensitivity (Successfully predict number {}): {:.5}".format(label,sens))
+    print("Specificity (Successfully predict not number {}): {:.5}".format(label,spec))
     print("")
     print("----------------------------------------")
+    return  acc,sens,spec
 
 def test_kernel(X,Y,classes):
     c_num = len(classes)
@@ -282,8 +368,11 @@ print('Total time cost : {}m , {:.3f}s'.format(min_c,time_c))
 # test_kernel(X_train,Y_train,classes)
 # sys.exit()
 print(X_train.shape,Y_train.shape ,"\n" , X_test.shape , Y_test.shape)
-SVM(X_train,Y_train,X_test,Y_test)
-sys.exit()
-theta = optimization(X,Y)
-GP(X,Y,theta)
+if is_SVM:
+    SVM(X_train,Y_train,X_test,Y_test)
+else:
+    theta = [1,1]
+    GP(X,Y,theta,"Gaussian Process (without optimization)")
+    theta = optimization(X,Y)
+    GP(X,Y,theta,"Gaussian Process (optimized)")
 plt.show()
